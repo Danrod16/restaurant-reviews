fbcee300b13de10b331c46388cb0dff107fd0ca3
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-11-10 10:32:51 +0100

    Feat: addded reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    # get '/top', to: 'restaurants#top'
+    collection do
+      get 'top'
+    end
+
+    member do
+      get 'chef'
+      # get '/chef', to: 'restaurants#chef'
+    end
+
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Clean database"
+Restaurant.destroy_all
+
+puts "Creating DB"
+100.times do
+  restaurant = Restaurant.create!(name: Faker::Restaurant.name , address: Faker::Address.street_address, stars: [1,2 ,3 ,4 ,5].sample, chef_name: ["Gordon Ramsey", "Paul Bocuse", "Enrique Olvera", "Thomas Keller"].sample)
+  puts "#{restaurant.name} created"
+end
+
+
+puts "#{Restaurant.all.count} created"
